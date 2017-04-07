866248e3092aae7204e1cdf49ab370e2ab02aaed
phil-mcdowall <philipsmcdowall@gmail.com>
2017-04-07 12:41:22 -0400

    initial commit

--- rp_demo.py
+++ rp_demo.py
@@ -13,11 +13,11 @@ if __name__ == '__main__':
     shapes = []
     for deg in range(0,360,1):
         profile = profiler.profile(source, radius, deg)
-        shapes.append(profile.line)
+        shapes.append((profile.line,deg))
         print(profile)
     print("Creating shapefile of profiles")
     schema = {'geometry': 'LineString', 'properties': {'angle': 'int'}}
     with collection('rp_demo.shp', "w", "ESRI Shapefile", schema, crs=ras._crs) as output:
         for line in shapes:
-            output.write({'geometry': mapping(line),
-                        'properties': {'angle': deg}})
+            output.write({'geometry': mapping(line[0]),
+                        'properties': {'angle': line[1]}})
